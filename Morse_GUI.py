d2981a2b73c76f864f5a12664c2c9d2088509df8
Samarth Mistry <103932039+GeekySamOP@users.noreply.github.com>
2022-12-02 23:30:14 +0530

    Update Morse_GUI.py

--- Morse_GUI.py
+++ Morse_GUI.py
@@ -1,9 +1,11 @@
 from time import sleep   # sleep is using for putting basic delay in our code
 from tkinter import *    # tkinter is a library to make and see GUI
+from tkinter import messagebox
 window = Tk()            # initialising gui named window
 from gpiozero import LED # gpiozero used for initialising of led
 import RPi.GPIO          #importing Genral input/output pin
 led = LED(16)
+LIMIT = 12
 MORSE_CODE_DICT = {         # morse code dictionary
     'A':'.-',
     'B':'-...',
@@ -49,7 +51,7 @@ def builder():                        # making function that translates alphabat
     arr = list(name)                #it will store all the characters into array/list
     MyWord = ''
     for letter in arr:             #this for-loop will convert letters to morse and join them as word
-        if letter != ' ':         # if letter was not space then 
+        if len(name) <=LIMIT and len(name)>0:         # if letter was not space then 
             MyWord = MORSE_CODE_DICT[letter] + ' ' #it will build the morse code
             var = list(MyWord)      
             print(MyWord)         #and print them
@@ -58,20 +60,28 @@ def builder():                        # making function that translates alphabat
                     led.on()
                     sleep(0.3)
                     led.off()
-                    sleep(0.3)
+                    sleep(0.5)
                 elif(var1 == "."):  # if sign is '.' then it will blink for 0.1 Second
                     led.on()
                     sleep(0.1)
                     led.off()
-                    sleep(0.1)
+                    sleep(0.5)
                 elif(var1 == " "): # if sign is ' ' then it will blink for 0.2 Second
                     led.on()
                     sleep(0.2)
                     led.off()
-                    sleep(0.2)
+                    sleep(0.5)
+        elif len(name) > LIMIT or len(name) == 0:
+            messagebox.showerror("Error", "Name cannot exceed " + str(LIMIT) )
+            var.set("")
+        elif len(name) == 0:
+            messagebox.showerror("Name cannot be null")
+            var.set("")
         else:
-            MyWord += ' '       
-    return MyWord       
+            MyWord += ' '
+
+    return MyWord     
+
 def close():
     window.destroy()        #if we click on button exit it will destroy the windows-3-
 
